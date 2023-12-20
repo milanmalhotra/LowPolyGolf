05c73b978ebfe27ee7b2dc4393336ac8b80579c0
milanmalhotra <milmalho@gmail.com>
2023-12-19 22:15:23 -0500

    player can walk up to ball and will enter into hitting phase/animation state

--- Assets/Scripts/GolfBall.cs
+++ Assets/Scripts/GolfBall.cs
@@ -1,14 +1,14 @@
-using Unity.Collections;
+using System;
+using Cinemachine;
 using UnityEngine;
 
 public class BallLaunch : MonoBehaviour
 {
     public Rigidbody golfBall;
-    // These will be determined by player
-    [SerializeField]
-    private float power;
+    public CinemachineVirtualCamera golfShotCamera;
+    public CinemachineVirtualCamera thirdPersonCamera;
     public float angle = 45f;
-    public bool inHittingPhase = true;
+    public float power;
     
     private float _maxIncrement = 30f;
     private float _incrementSpeed = 10f;
@@ -16,44 +16,51 @@ public class BallLaunch : MonoBehaviour
     private bool _isIncrementing = false;
     private bool _isDecrementing = false;
     private bool _isReadyToLaunch = false;
-
+    private bool _inHittingPhase;
+    
     void Update()
     {
-        if (inHittingPhase) {
+        _inHittingPhase = GameVariables.getHittingPhase();
+        if (_inHittingPhase) {
             if (_isReadyToLaunch) {
                 InitiateLaunch();
                 _isReadyToLaunch = false;
             }
 
-            if (_isIncrementing) {
-                // Increment the float value until it reaches the maximum
-                if (power < _maxIncrement && !_isDecrementing) {
-                    power += _incrementSpeed * Time.deltaTime;
-                } else {
-                    _isDecrementing = true;
-                    // If the value reaches the maximum, start decreasing
-                    power -= _decreaseSpeed * Time.deltaTime;
-                    if (power <= 0f) {
-                        // Stop incrementing once it returns to 0
-                        power = 0f;
-                        _isIncrementing = false;
-                    }
-                }
+            if (_isIncrementing)
+            {
+                IncrementPower();
             }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Golfer")
+        {
+            GameVariables.setHittingPhase(true);
+            Animator golferAnimator = other.gameObject.GetComponent<Animator>();
+            golferAnimator.SetLayerWeight(1, 0);
+            golferAnimator.SetBool("inSetup", true);
+            golfShotCamera.gameObject.SetActive(true);
+            thirdPersonCamera.gameObject.SetActive(false);
         }
     }
 
     // Called whenever user clicks left mouse button
     public void OnLaunchBall()
     {
-        if (_isIncrementing) {
-            // Save the current value and stop incrementing
-            _isIncrementing = false;
-            _isReadyToLaunch = true;
-        } else {
-            // Reset values and start incrementing
-            power = 0f;
-            _isIncrementing = true;
+        if (_inHittingPhase)
+        {
+            if (_isIncrementing) {
+                // Save the current value and stop incrementing
+                _isIncrementing = false;
+                _isReadyToLaunch = true;
+            } else {
+                // Reset values and start incrementing
+                power = 0f;
+                _isIncrementing = true;
+            }
         }
     }
 
@@ -67,5 +74,22 @@ public class BallLaunch : MonoBehaviour
     
         // Apply force to the golf ball based on direction and power
         golfBall.AddForce(shotDirection * power, ForceMode.Impulse);
+    }
+
+    void IncrementPower()
+    {
+        // Increment the float value until it reaches the maximum
+        if (power < _maxIncrement && !_isDecrementing) {
+            power += _incrementSpeed * Time.deltaTime;
+        } else {
+            _isDecrementing = true;
+            // If the value reaches the maximum, start decreasing
+            power -= _decreaseSpeed * Time.deltaTime;
+            if (power <= 0f) {
+                // Stop incrementing once it returns to 0
+                power = 0f;
+                _isIncrementing = false;
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,35 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.TextCore.Text;
 
 public class PlayerController : MonoBehaviour
 {
-    public LayerMask groundLayer;
     [Range(1, 20)]
     public float sensitivity = 1f;
-
-    private PlayerInput playerInput;
-    private Animator animator;
-    private GameObject test;
+    
+    private PlayerInput _playerInput;
+    private Animator _animator;
     private Vector3 _moveDirection;
     private float _groundCheckDistance = 1.5f;
-    
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = gameObject.GetComponent<Animator>();
-        playerInput = gameObject.GetComponent<PlayerInput>();
+        _animator = gameObject.GetComponent<Animator>();
+        _playerInput = gameObject.GetComponent<PlayerInput>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        GameVariables.setHittingPhase(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Move();
-        Rotate();
+        bool inHittingPhase = GameVariables.getHittingPhase();
+        if (!inHittingPhase)
+        {
+            Move();
+            Rotate();
+        }
     }
 
     /*
@@ -39,30 +37,30 @@ public class PlayerController : MonoBehaviour
     * TODO: make controller not have to hold down left stick click, just on click sets bool to the opposite
     */
     void Move() {
-        bool isWalking = playerInput.actions["Move"].IsInProgress();
-        bool isSprinting = playerInput.actions["Sprint"].IsInProgress();
+        bool isWalking = _playerInput.actions["Move"].IsInProgress();
+        bool isSprinting = _playerInput.actions["Sprint"].IsInProgress();
 
         if (isWalking && !isSprinting) {
-            animator.SetBool("isWalking", true);
+            _animator.SetBool("isWalking", true);
         } 
 
         if (!isWalking) {
-            animator.SetBool("isWalking", false);
+            _animator.SetBool("isWalking", false);
         }
 
         if (isSprinting) {
-            animator.SetBool("isSprinting", true);
-            animator.SetBool("isWalking", false);
+            _animator.SetBool("isSprinting", true);
+            _animator.SetBool("isWalking", false);
         }
 
         if (!isSprinting) {
-            animator.SetBool("isSprinting", false);
+            _animator.SetBool("isSprinting", false);
         }
     }
 
     // Method handles players rotation based off of mouse movements
     void Rotate() {
-        Vector2 lookInput = playerInput.actions["Look"].ReadValue<Vector2>();
+        Vector2 lookInput = _playerInput.actions["Look"].ReadValue<Vector2>();
         float mouseX = lookInput.x;
 
         transform.Rotate(Vector3.up * mouseX * sensitivity / 150);
